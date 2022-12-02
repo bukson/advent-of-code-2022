466ec6b2fb951b3ad56c318b255bc57db9fc3299
Michał Bukowski <michal.bukowski@i4-insight.com>
2022-12-02 16:47:44 +0100

    Solved 01 Part 1

---
+++ 01/Part_1/solution.hs
@@ -0,0 +1,21 @@
+import qualified Data.Text    as Text
+import qualified Data.Text.IO as Text
+
+readInputFile :: IO [String]
+readInputFile = do
+    input_lines <- fmap Text.lines (Text.readFile "input.txt")
+    return $ map Text.unpack input_lines
+
+splitStrList :: [String] -> [[Int]]
+splitStrList strList = splitStrListAcc strList [] [[]]
+
+splitStrListAcc :: [String] -> [Int] -> [[Int]] -> [[Int]]
+splitStrListAcc ("":listTail) acc1 acc2 = splitStrListAcc listTail [] (acc1:acc2)
+splitStrListAcc (str:listTail) acc1 acc2 = splitStrListAcc listTail ((read str):acc1) acc2
+splitStrListAcc [] acc1 acc2 = (acc1:acc2)
+
+-- main :: IO Int
+main = do
+    strList <- readInputFile
+    let sums = map sum (splitStrList strList)
+    return $ foldl (\acc i -> max acc i) 0 sums
